4aa931fa4ba26fca5b86b4dd2072af69895b4178
Grace Choi <oiograceoio@gmail.com>
2019-04-03 20:26:52 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -29,4 +29,9 @@ class ApplicationController < Sinatra::Base
     redirect to '/recipes'
   end
 
+  get '/recipes/:id/edit' do
+    @recipe = Recipe.find_by_id(params[:id])
+    erb :edit 
+  end
+
 end
